4ebc7fb511c9eb4e4a1494a4e9f8f4b1884295c9
superfony <fony2011@163.com>
2015-07-31 14:09:43 +0800

    2015年07月31日14:09:36

--- wZDP/src/main/java/com/epsmart/wzcc/activity/MainActivity.java
+++ wZDP/src/main/java/com/epsmart/wzcc/activity/MainActivity.java
@@ -19,22 +19,23 @@ import com.epsmart.wzcc.updata.UpdateManager;
 import com.epsmart.wzcc.view.CircleImageView;
 
 /**
- *首页显示
+ * 首页显示
  */
 public class MainActivity extends ClientActivity {
-	private String TAG = MainActivity.class.getName();
-	public static String provenance;
-	private BaseHttpModule httpModule;
-	private Activity activity;
-	private CircleImageView supply;// 供应商现场管理
-	private CircleImageView online;// 在线分析
-	private PackageManager pm = null;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		activity = this;
-		setContentView(R.layout.activity_main);
-		UpdateManager.getUpdateManager().checkAppUpdate(this, false);// 检查是否更新
+    private String TAG = MainActivity.class.getName();
+    public static String provenance;
+    private BaseHttpModule httpModule;
+    private Activity activity;
+    private CircleImageView supply;// 供应商现场管理
+    private CircleImageView online;// 在线分析
+    private PackageManager pm = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = this;
+        setContentView(R.layout.activity_main);
+        UpdateManager.getUpdateManager().checkAppUpdate(this, false);// 检查是否更新
 //		ImageView install = (ImageView) findViewById(R.id.install_image);// 设置按钮
 //
 //		install.setOnClickListener(new OnClickListener() {
@@ -46,62 +47,59 @@ public class MainActivity extends ClientActivity {
 //		});
 
 
+    }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-	}
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            dialog();
+            return false;
+        }
+        return false;
 
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
+    }
 
-		if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
-			dialog();
-			return false;
-		}
-		return false;
+    // 退出弹出框
+    Dialog dialog;
 
-	}
+    private void dialog() {
 
-	// 退出弹出框
-	Dialog dialog;
+        LayoutInflater inflater = LayoutInflater.from(activity);
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(
+                R.layout.exit_dialog, null);
 
-	private void dialog() {
+        dialog = new AlertDialog.Builder(activity).create();
+        dialog.setCancelable(false);
+        dialog.show();
+        dialog.getWindow().setContentView(layout);
 
-		LayoutInflater inflater = LayoutInflater.from(activity);
-		RelativeLayout layout = (RelativeLayout) inflater.inflate(
-				R.layout.exit_dialog, null);
+        Button exit_cancel = (Button) layout.findViewById(R.id.exit_cancel);
+        Button drop_out = (Button) layout.findViewById(R.id.drop_out);
 
-		dialog = new AlertDialog.Builder(activity).create();
-		dialog.setCancelable(false);
-		dialog.show();
-		dialog.getWindow().setContentView(layout);
+        drop_out.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                activity.finish();
+            }
+        });
+        exit_cancel.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
 
-		Button exit_cancel = (Button) layout.findViewById(R.id.exit_cancel);
-		Button drop_out = (Button) layout.findViewById(R.id.drop_out);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
-		drop_out.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				dialog.dismiss();
-				activity.finish();
-				 //TODO
-			}
-		});
-		exit_cancel.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				dialog.dismiss();
-			}
-		});
-	}
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-
-	}
+    }
 }
