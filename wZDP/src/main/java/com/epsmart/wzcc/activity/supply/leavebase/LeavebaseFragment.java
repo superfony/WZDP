d1ca3dd31bcbff51ed3a98fd299b792413703acb
tete999 <1144363620@qq.com>
2015-09-11 18:07:25 +0800

    2015-9-11 16:30

--- wZDP/src/main/java/com/epsmart/wzcc/activity/supply/leavebase/LeavebaseFragment.java
+++ wZDP/src/main/java/com/epsmart/wzcc/activity/supply/leavebase/LeavebaseFragment.java
@@ -3,7 +3,6 @@ package com.epsmart.wzcc.activity.supply.leavebase;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -172,7 +171,7 @@ public class LeavebaseFragment extends CommonFragment {
                 requestPram.param = req;
                 paginationWidget.getRequestAction().reset();
                 processQueryCondition(null);
-                paginationWidget.loadPaginationDatas();
+                paginationWidget.loadPaginationData();
             }
         };
     }
